47dbc91d25470affa5e9398151fdff1ffd335e04
Erik Pablo <nunespablo470@gmail.com>
2025-06-11 00:12:46 -0300

    feat: :sparkles: created environment variables

---
+++ src/env/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.log('❌Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables.') // Usado para nao passar daqui caso der error
+}
+
+export const env = _env.data
