051b21ef32c502b07501f2c4ab27d936ef544bc2
Erik Pablo <nunespablo470@gmail.com>
2025-06-17 09:48:51 -0300

    feat: :sparkles: create route users

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,40 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
 
-prisma.user.create({
-  data: {
-    name: 'Erik Pablo',
-    email: 'nunes@gmail.com',
-  },
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
 })
+
+/**
+ * Criamos uma pasta para a conexao do banco
+ * exportamos para dentro do appp
+ *
+ * no appp
+ *
+ * criamos a rota para cria usuarios
+ * onde o zod ira validar os dados, e o o prisma.user.create, criamos
+ * o usuario.
+ * retornamos o 201
+ *
+ * e pór meio do insominia criamos o user.
+ * e pelo prisma studio podemos ver.
+ */
