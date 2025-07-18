0202e8c7e0d2f5871268e27fe875c269a77153ec
Erik Pablo <nunespablo470@gmail.com>
2025-07-18 09:21:58 -0300

    feat: :sparkles: create route authenticate

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 import { register } from './controller/register'
+import { authenticate } from './controller/autheticate'
 
 export async function appRouter(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 }
