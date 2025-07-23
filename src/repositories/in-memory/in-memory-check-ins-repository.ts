dacb0f40961e8a6b4d0efa3e5100c04110473ace
Erik Pablo <nunespablo470@gmail.com>
2025-07-23 09:37:52 -0300

    feat: :sparkles: check ins

---
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -0,0 +1,21 @@
+import type { CheckIn, Prisma } from '@prisma/client'
+import type { CheckInsRepository } from '../check-ins-repository'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryChecksInsRepository implements CheckInsRepository {
+  public items: CheckIn[] = []
+
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = {
+      id: randomUUID(),
+      user_id: data.user_id,
+      gym_Id: data.gym_Id,
+      validated_at: data.validated_at ? new Date(data.validated_at) : null,
+      created_at: new Date(),
+    }
+
+    this.items.push(checkIn)
+
+    return checkIn
+  }
+}
