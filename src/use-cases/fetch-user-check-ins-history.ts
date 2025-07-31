3fa56b6c503799ad6040f951d6d43fb37a7032ec
Erik Pablo <nunespablo470@gmail.com>
2025-07-30 22:39:24 -0300

    feat: :sparkles: cresate use case history ckeck in

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,26 @@
+import type { CheckInsRepository } from '@/repositories/check-ins-repository'
+import type { CheckIn } from '@prisma/client'
+
+interface FetchUserCheckInsHistoryUseCaseRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUserCheckInsHistoryUseCaseResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistoryUseCase {
+  constructor(private checkInRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
+    const checkIns = await this.checkInRepository.findManyByUserId(userId, page)
+
+    return {
+      checkIns,
+    }
+  }
+}
