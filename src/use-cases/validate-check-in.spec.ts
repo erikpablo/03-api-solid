de6699a2d0e070b05cb468b084fa31d265dd3660
Erik Pablo <nunespablo470@gmail.com>
2025-08-07 09:51:43 -0300

    feat: :sparkles: validade check use case

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -11,11 +11,11 @@ describe('Validate CheckIns Use Case', () => {
     checkInsRepository = new InMemoryChecksInsRepository()
     sut = new ValidateCheckInUseCase(checkInsRepository)
 
-    // vi.useFakeTimers()
+    vi.useFakeTimers()
   })
 
   afterEach(() => {
-    // vi.useRealTimers()
+    vi.useRealTimers()
   })
 
   it('should be able to validate the check-in', async () => {
@@ -38,5 +38,22 @@ describe('Validate CheckIns Use Case', () => {
         checkInId: 'inexistent-check-in-id',
       })
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+    const createdCheckIn = await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_Id: 'gym-01',
+    })
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+    vi.advanceTimersByTime(twentyOneMinutesInMs) // toda que o new Data for chmado, ele vai avançar 21 minutos
+
+    expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      })
+    ).rejects.toBeInstanceOf(Error)
   })
 })
