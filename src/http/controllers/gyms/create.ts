93e4b08d1f75425a9551c722644f62e73bdb5eee
Erik Pablo <nunespablo470@gmail.com>
2025-08-29 07:55:57 -0300

    feat: :sparkles: route create gmys

---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,27 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeCreateGymUseCase } from '@/use-cases/factories/make-create-gym-use-case'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const createGymsBodySchema = z.object({
+    title: z.string(),
+    description: z.string().nullable(),
+    phone: z.string().nullable(),
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+      // refine é usado para criar uma validação customizada
+      // Math.abs() retorna o valor absoluto, ou seja, positivo
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { title, description, phone, latitude, longitude } =
+    createGymsBodySchema.parse(request.body)
+
+  const createGyms = makeCreateGymUseCase()
+
+  await createGyms.execute({ title, description, phone, latitude, longitude })
+  return reply.status(201).send()
+}
